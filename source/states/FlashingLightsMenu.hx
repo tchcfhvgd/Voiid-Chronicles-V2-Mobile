0244a7905a1a0630fbb25a90af90e51f72ec9b3d
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-03-22 18:33:57 +0800

    Update FlashingLightsMenu.hx

--- source/states/FlashingLightsMenu.hx
+++ source/states/FlashingLightsMenu.hx
@@ -15,11 +15,10 @@ class FlashingLightsMenu extends MusicBeatState
         #if mobile 
         var t = "Hey! Leather Engine has flashing lights\nPress Y to enable them, or anything else to not.\n(Any key closes this menu)\n\n";
         t += "This is a Mobile port of Voiid Chonicles,\nsome things may not work correctly\nor some songs maybe crash depending on your device.\n";
-        t += "Shaders and Modcharts are disabled by default,\nyou can turn them back on but it may crash on some songs,\nif youre still getting crashes check\nGraphics->Optimization in the options menu and mess with the settings\n";
+        t += "Shaders are disabled by default,\nyou can turn them back on but it may crash on some songs,\nif youre still getting crashes check\nGraphics->Optimization in the options menu and mess with the settings\n";
         text.size = 24;
         text.text = t;
-        Options.setData(false, "shaders"); //turned off by default      
-        Options.setData(false, "modcharts");
+        Options.setData(false, "shaders"); //turned off by default
         Options.setData(false, "gpuTextures");   
         #end
         text.font = Paths.font("vcr.ttf");
@@ -33,10 +32,13 @@ class FlashingLightsMenu extends MusicBeatState
         super.update(elapsed);
 
         var pressed:Bool = false;
-        #if mobile
-		if (MobileControls.justPressedAny())
-            pressed = true;
-		#end
+        for (touch in FlxG.touches.list)
+		{
+			if (touch.justPressed)
+			{
+				pressed = true;
+			}
+		}
 
         if(FlxG.keys.justPressed.Y)
             Options.setData(true, "flashingLights");
